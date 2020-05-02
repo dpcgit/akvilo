197f502a87b49bb3a8d6d53ad83d4c5bc79ad01c
Daniel Puentes <daniel.puentes.ca@tutanota.com>
2020-05-01 21:12:03 -0500

    Playing with components, no state or props yet

--- myApp/src/pages/Tab1.tsx
+++ myApp/src/pages/Tab1.tsx
@@ -1,23 +1,38 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonText, IonButton, IonFab, IonIcon, IonFabButton} from '@ionic/react';
+import {addCircle} from 'ionicons/icons'
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
-const Tab1: React.FC = () => {
+const Tab1: React.FC = (props) => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Consumo de agua</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
+
+	<IonInput type="number" placeholder="Ejemplo, 2000 ml" max="5000" min="0">
+	Ingresa la cantidad de agua a tomar
+	</IonInput>
+
+	<IonButton>Fijar
+	</IonButton>
+
+	<IonFab vertical="bottom" horizontal="end">
+	 <IonFabButton>
+	  <IonIcon icon={addCircle} size="large">
+	  </IonIcon>
+	 </IonFabButton>
+	</IonFab>
+	
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
   );
