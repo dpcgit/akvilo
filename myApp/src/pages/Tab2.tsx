8a4416d0752dbd4b7ea4cb7f912582414d6cf4a5
Daniel Puentes <daniel.puentes.ca@tutanota.com>
2020-05-08 21:47:39 -0500

    Added segment buttons for statistics

--- myApp/src/pages/Tab2.tsx
+++ myApp/src/pages/Tab2.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar , IonSegment, IonSegmentButton} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
@@ -12,12 +12,11 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+       <IonSegment>
+	<IonSegmentButton>Semana</IonSegmentButton>
+	<IonSegmentButton>Mes</IonSegmentButton>
+	<IonSegmentButton>Año</IonSegmentButton>	
+       </IonSegment>
       </IonContent>
     </IonPage>
   );
